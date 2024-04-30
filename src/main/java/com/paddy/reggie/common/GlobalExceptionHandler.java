68192adeceb092d57e442f9a24be30e1f2b71294
Paddy_Pan <pdd337816464@outlook.com>
2024-04-30 21:07:37 +0800

    feat: 完成新增员工，增加全局异常处理

---
+++ src/main/java/com/paddy/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.paddy.reggie.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理器
+ *  拦截带 @RestController的 controller
+ *
+ *  ResponseBody -> 返回 Json 数据
+ */
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+    /**
+     * 异常处理
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+
+        if (ex.getMessage().contains("Duplicate entry")){
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("未知错误");
+    }
+}
--- src/main/java/com/paddy/reggie/controller/EmployeeController.java
+++ src/main/java/com/paddy/reggie/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -77,8 +78,20 @@ public class EmployeeController {
      */
     // RequestBody -> json 格式
     @PostMapping
-    public R<String> save(@RequestBody Employee employee){
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee){
         log.info("新增员工，员工信息{}", employee.toString());
-        return null;
+
+        // 设置初始密码
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+        return R.success("新增员工成功");
     }
 }
