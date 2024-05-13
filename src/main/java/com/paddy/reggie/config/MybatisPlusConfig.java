b1fc82cd7a41cacd2c317c0e07ad139f1cbe0484
paddy <pdd337816464@163.com>
2024-05-13 19:30:21 +0800

    MybatisPlus分页插件的配置

---
+++ src/main/java/com/paddy/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,17 @@
+package com.paddy.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 配置mybatisPlus的分页插件
+ */
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
