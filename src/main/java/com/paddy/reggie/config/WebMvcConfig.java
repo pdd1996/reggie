eb720015db3595c4094581598282ea1c1d092c2c
paddy <pdd337816464@163.com>
2024-05-16 20:10:19 +0800

    feat:扩展MVC框架的消息转换器和对象映射器

--- src/main/java/com/paddy/reggie/config/WebMvcConfig.java
+++ src/main/java/com/paddy/reggie/config/WebMvcConfig.java
@@ -1,9 +1,15 @@
 package com.paddy.reggie.config;
 
+import com.paddy.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
 @Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
@@ -19,4 +25,17 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
 
+    /**
+     * 扩展MVC框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //创建消息转换器
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //消息转换器设置具体的对象映射器
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //替换原来的默认消息转换器
+        converters.add(0,messageConverter);
+    }
 }
